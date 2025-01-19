2c0aafb78610cdd3bf7545522ef74f7303e94437
Curry5004 <yunjin5004@naver.com>
2025-01-19 15:51:43 +0900

    * 25. 01. 19
    스프링 강의 자료 구축

---
+++ src/main/java/hello/servlet/SpringBasicMvcPart1Application.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicMvcPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicMvcPart1Application.class, args);
+    }
+
+}
