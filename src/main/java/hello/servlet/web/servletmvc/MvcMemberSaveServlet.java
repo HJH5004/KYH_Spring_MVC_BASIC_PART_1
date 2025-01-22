6f318da515757441297020ee4cf372d651879135
Curry5004 <yunjin5004@naver.com>
2025-01-23 07:52:57 +0900

    * 25. 01. 23
    mvc 패턴을 도입한 웹 서비스 구축
    
    기본적인 frontController 구조를 사용한 MVC 페턴

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,41 @@
+package hello.servlet.web.servletmvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+
+        memberRepository.save(member);
+
+        /**
+         * 원래는 jsp 뷰가 있어야 하는데 모델에 이를 보관해준다.
+         * */
+
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/save-result.jsp
@@ -0,0 +1,26 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: yunji
+  Date: 2025-01-22
+  Time: 오전 7:31
+  To change this template use File | Settings | File Templates.
+--%>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+앙 성공
+
+<ul>
+    <li>id = ${member.id}</li>
+    <li>username = ${member.username}</li>
+    <li>age = ${member.age}</li>
+
+</ul>
+
+
+<a href="/index.html">메인</a>
+</body>
+</html>
